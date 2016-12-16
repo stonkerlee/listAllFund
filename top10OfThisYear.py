9fbb071e031a338c094442cf96a2c85f86af296b
RayLee <stonker.lee@gmail.com>
2016-12-16 11:41:40 +0800

    Divide by HH and GP

--- top10OfThisYear.py
+++ top10OfThisYear.py
@@ -1,15 +1,30 @@
+# coding=utf-8
+
 import urllib2
 
-def top10OfThisYear():
-    URL = 'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=all&sc=jnzf&st=desc&pi=1&pn=10&dx=1'
+msg = ''
+
+def top10OfThisYear(type):
+    global msg
+    URL = 'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft={0}&sc=jnzf&st=desc&pi=1&pn=10&dx=1'.format(type)
     content = urllib2.urlopen(URL).read()
     start = content.find('[')
     end = content.find(']')
     data = content[start+2 : end-1]
     L = data.split('","')
     for e in L:
+        # print e
         r = e.split(',')
-        print('{0} {1}% {2}'.format(r[0], r[14], r[1]))
+        # print('{0} {1:>7}% {2} {3}'.format(r[0], r[14], r[3], r[1]))
+        msg += '{0} {1:>7}% {2} {3}\n'.format(r[0], r[14], r[3], r[1])
 
 
-top10OfThisYear()
+
+if __name__ == '__main__':
+    # print('-'*10 + 'top10 股票型' + '-'*10)
+    msg += '-'*10 + 'top10 股票型' + '-'*10 + '\n'
+    top10OfThisYear('gp')
+    # print('-'*10 + 'top10 混合型' + '-'*10)
+    msg += '-'*10 + 'top10 混合型' + '-'*10 + '\n'
+    top10OfThisYear('hh')
+    print(msg)
