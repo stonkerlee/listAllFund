a905502cb5e2554a5e0c8458d463ac3baf5f9460
RayLee <stonker.lee@gmail.com>
2016-12-30 17:17:38 +0800

    Add module littlebeanfund for future

---
+++ littlebeanfund.py
@@ -0,0 +1,138 @@
+# coding=utf-8
+
+"""
+dt = Data Type?  kf = '开放式基金'?
+ft = Fund Type?
+st=asc 升序排序
+sd=2015-12-08&ed=2016-12-08
+"""
+
+import urllib2
+import re
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def get_fundvalue_history(fund_id):
+    """
+    Get history values of the specified fund
+    :param fund_id: for example, '000831'
+    :return: (fund_id, value_list)
+    """
+    url_template = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code={0}&per=100000'
+    url = url_template.format(fund_id)
+    content = urllib2.urlopen(url).read()
+
+    # get total records
+    records_regex = re.compile('records:(\d)+')
+    mo = records_regex.search(content)
+    total_records = mo.group().split(':')[1]
+    logging.log(logging.DEBUG, fund_id + ' has records: ' + total_records)
+
+    raw_data = convert_f10content_to_rawdata(content)
+    # parse rows
+    rows_str = raw_data.split('</tr>')
+    rows_str.remove('')
+
+    result = []
+    for row in rows_str:
+        row_elements = row.split('</td>')
+        row_elements.remove('')
+        row_result = []
+        for i in range(len(row_elements)):
+            e = row_elements[i].split('>')[-1]
+            row_result.append(e)
+        result.append(tuple(row_result))
+    return fund_id, result
+
+
+def get_fundvalue_latest(fund_id):
+    """
+    Get latest value of the specified fund
+    :param fund_id: for example, '000831'
+    :return: (fund_id, value)
+    """
+    url_template = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code={0}&per=1'
+    url = url_template.format(fund_id)
+    content = urllib2.urlopen(url).read()
+
+    raw_data = convert_f10content_to_rawdata(content)
+    elements = raw_data.split('</td>')
+    del elements[-1]
+    # logging.log(logging.DEBUG, elements)
+    result = [e.split('>')[-1] for e in elements]
+    logging.log(logging.DEBUG, result)
+    return fund_id, result
+
+
+def get_fundvalue_bydate(fund_id, date):
+    """
+    Get value of the specified fund at the specified date
+    :param fund_id: for example, '000831'
+    :param date: the specified date
+    :return: (fund_id, value)
+    """
+    url_template = 'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code={0}&sdate={1}&edate={1}'
+    url = url_template.format(fund_id, date)
+    content = urllib2.urlopen(url).read()
+
+    raw_data = convert_f10content_to_rawdata(content)
+    elements = raw_data.split('</td>')
+    if len(elements) < 3:
+        return fund_id, None
+    else:
+        return fund_id, [e.split('>')[-1] for e in elements]
+
+
+def get_funds():
+    """
+    Get a list of all funds
+    :return: list of alll funds
+    """
+    url = 'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=all&st=asc&pi=1&pn=5000'
+    content = urllib2.urlopen(url).read()
+
+    # get number of funds
+    allrecords_regex = re.compile('allRecords:(\d)+')
+    mo = allrecords_regex.search(content)
+    number_of_record = mo.group().split(':')[1]
+    logging.log(logging.DEBUG, 'Number of all funds is ' + number_of_record)
+
+    raw_data = convert_rankcontent_to_rawdata(content)
+    return construct_rankrawdata_to_list(raw_data)
+
+
+# The following are helper functions
+def convert_f10content_to_rawdata(content):
+    begin = content.find('<tbody>') + len('<tbody>')
+    end = content.find('</tbody>')
+    return unicode(content[begin:end], encoding='utf-8')
+
+
+def convert_rankcontent_to_rawdata(content):
+    begin = content.find('[') + len('[')
+    end = content.find(']')
+    return unicode(content[begin:end], encoding='utf-8')
+
+
+def construct_rankrawdata_to_list(raw_data):
+    temp = raw_data.replace('","', '"?"').split('?')
+    return [(x[0], x[1]) for x in [e.split(',') for e in temp]]
+
+
+
+if __name__ == '__main__':
+    # print get_fundvalue_latest('000831')
+    # print get_fundvalue_latest('001076')
+    #
+    # print get_fundvalue_bydate('000831', '2016-12-28')
+    # print get_fundvalue_bydate('000831', '2016-12-30')
+
+    # print get_fundvalue_history('000815')
+
+    print get_funds()
+    # my = '"a,b,c","aa,bb,cc","aaa,bbb,ccc"'
+    # my = my.replace('","', '"?"')
+    # print my.split('?')
